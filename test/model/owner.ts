53d95468bff2baaa3b90a735c1f33175566a81f2
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-12-10 15:42:56 +0700

    chore: testing components

---
+++ test/model/owner.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema } from "../../src"
+import { Key } from "../../src/util"
+
+export class OwnerContact {
+  @Prop({
+    validate: (v: string) => /^\\+[1-9][0-9]{7,14}$/.test(v),
+    validateMessage: "must be valid phone number",
+  })
+  phone: string
+
+  @Prop({
+    validate: (v: string) =>
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v),
+    validateMessage: "must be valid email",
+  })
+  email: string
+}
+
+@Schema({
+  collection: "owner",
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    deletedAt: "deleted_at",
+  },
+})
+// @Unique(["email"], { name: "uniq_users_email" })
+// @Unique(["username"])
+export class Owner {
+  @Prop({
+    required: true,
+    transform: (value: string) => `CatLover ${value.trim()}`,
+  })
+  name: string
+
+  @Prop()
+  @Key({ prefix: "username::" })
+  username: string
+
+  @Prop({ type: () => OwnerContact })
+  contact: OwnerContact
+}
